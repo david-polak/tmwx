b9c157c5c486b686d6184f0d2f54963a72f8e8b1
David Polák <git@polakdavid.com>
2022-07-24 13:53:40 +0200

    fix: [ci skip] 269b672f-0c83-4f32-9776-be104937e4c0 dev-0001 x

--- src/cache.rs
+++ src/cache.rs
@@ -1,5 +1,7 @@
-// use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+use crate::utils::get_epoch;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct CacheRecord {
@@ -7,11 +9,32 @@ pub struct CacheRecord {
     data: String,
 }
 
-// #[derive(Debug)]
-// pub struct Cache {
-//     taf: HashMap<String, CacheRecord>,
-//     metar: HashMap<String, CacheRecord>,
-// }
+#[derive(Debug)]
+pub struct Cache {
+    map: HashMap<String, CacheRecord>,
+}
+
+impl Cache {
+    pub fn new() -> Self {
+        Self {
+            map: HashMap::new(),
+        }
+    }
+
+    pub fn insert(&mut self, icao: String, value: String) {
+        let record = CacheRecord {
+            timestamp: get_epoch(),
+            data: value,
+        };
+
+        self.map.insert(icao, record);
+    }
+
+    pub fn get(&self, icao: &String) -> Option<&CacheRecord> {
+        self.map.get(icao)
+    }
+}
+
 //
 // impl Cache {
 //     // let taf = HashMap::new();
@@ -24,15 +47,23 @@ pub struct CacheRecord {
 //     }
 // }
 //
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//
-//     #[test]
-//     fn test_cache_1() {
-//         // println!("{}", get_epoch());
-//         let cache = Cache::new();
-//         // let record =
-//         // cache.taf.insert("LKPR", )
-//     }
-// }
+#[cfg(test)]
+mod cache_tests {
+    use super::*;
+
+    #[test]
+    fn test_cache_inserts_records() {
+        let mut cache = Cache::new();
+        let expected1 = String::from("expected data 1");
+        let icao1 = String::from("LKPR");
+
+        let expected2 = String::from("expected data 2");
+        let icao2 = String::from("LKHK");
+
+        cache.insert(icao1.clone(), expected1.clone());
+        cache.insert(icao2.clone(), expected2.clone());
+
+        assert!(Some(cache.map.get(&icao1)).is_some());
+        assert!(Some(cache.map.get(&icao2)).is_some());
+    }
+}
